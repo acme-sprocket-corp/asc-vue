863eeae5f6718c8713389e92fb339a30df872d13
Michael Bradvica <mjbradvica@gmail.com>
2023-05-19 19:44:30 -0400

    config and lint

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../features/home/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../features/home/Home.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "home",
+      component: Home,
     },
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
