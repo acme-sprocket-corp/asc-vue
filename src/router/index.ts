f3ed0900706475e25bf11fce7af781af6d98bef1
Michael Bradvica <mjbradvica@protonmail.com>
2023-05-20 20:35:31 -0400

    needs a le fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
 import homeRoutes from "@/features/home/Home.routes";
+import HomeView from "@/features/home/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [homeRoutes()],
+  routes: [
+    homeRoutes(),
+    {
+      path: "/idk/:id",
+      name: "myBffJill",
+      component: HomeView,
+      props: (route): { id: number } => ({
+        id: Number.parseInt("1"),
+      }),
+    },
+  ],
 });
 
 export default router;
